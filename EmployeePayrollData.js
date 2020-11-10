dbdc17b08548402f8b06441a8825dbb5cc3cbbf3
dsanimm <dsanimm@gmail.com>
2020-11-10 22:43:12 +0530

    extend employee payroll class

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -2,12 +2,16 @@ class EmployeePayrollData {
     //Properties
     id;
     salary;
+    gender;
+    startDate;
 
     //Constructor
-    constructor(id, name, salary) {
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
+    constructor(...params) {
+        this.id = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4];
     }
 
     //Getters & Setters
@@ -20,7 +24,10 @@ class EmployeePayrollData {
 
     //Methods
     toString() {
-        return "id : " + this.id + " Name : " + this.name + " Salary : " + this.salary;
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const empDate = this.startDate === undefined ? "Undefined" : this.startDate.toLocaleDateString("en-US", options);
+        return "id : " + this.id + " Name : " + this.name + " Salary : "
+            + this.salary + " Gender : " + this.gender + " Start Date : " + empDate;
     }
 }
 
@@ -28,3 +35,5 @@ let empData = new EmployeePayrollData(1, "Deepanshu", 10000);
 console.log(empData.toString());
 empData.name = "Deepanshu Singh";
 console.log(empData.toString());
+let empData2 = new EmployeePayrollData(2, "Kiran", 60000, "M", new Date());
+console.log(empData2.toString());
