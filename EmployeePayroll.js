d6a07f841de41f4c27c63a9e81131b87c78e20fa
dsanimm <dsanimm@gmail.com>
2020-11-10 21:47:40 +0530

    calculating wages for a month

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -1,10 +1,12 @@
-//UC3 calculates the daily wage by function
+
+//UC4 calculates the monthly wage
 const IS_ABSENT = 0;
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_DURATION = 4;
 const FULL_TIME_DURATION = 8;
 const EMP_WAGE_PER_HOUR = 20;
+const WORKING_DAYS_IN_MONTH = 20;
 function getDailyEmployeeHours(empCheck) {
     let dailyHours = 0;
     switch (empCheck) {
@@ -18,7 +20,13 @@ function getDailyEmployeeHours(empCheck) {
     }
     return dailyHours;
 }
-empCheck = Math.floor(Math.random() * 10) % 3;
-let dailyHours = getDailyEmployeeHours(empCheck);
-let dailyWage = dailyHours * EMP_WAGE_PER_HOUR;
-console.log('Employee Daily Wage is : ' + dailyWage);
+let totalHoursInMonth = 0;
+for (i = 0; i < WORKING_DAYS_IN_MONTH; i++) {
+    empCheck = Math.floor(Math.random() * 10) % 3;
+    let dailyHours = getDailyEmployeeHours(empCheck);
+    let dailyWage = dailyHours * EMP_WAGE_PER_HOUR;
+    console.log('Employee Daily Wage is : ' + dailyWage);
+    totalHoursInMonth += dailyHours;
+}
+console.log('Employee Total Wage In A Month : ' + totalHoursInMonth * EMP_WAGE_PER_HOUR);
+
