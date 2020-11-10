0982162dcf498902d02d22c2854d7d8201a5aea6
dsanimm <dsanimm@gmail.com>
2020-11-10 21:50:15 +0530

    calculating wages till num of working days or total working hours/month met

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -1,5 +1,4 @@
-
-//UC4 calculates the monthly wage
+//UC5 calculates the monthly wage for conditions
 const IS_ABSENT = 0;
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
@@ -7,6 +6,7 @@ const PART_TIME_DURATION = 4;
 const FULL_TIME_DURATION = 8;
 const EMP_WAGE_PER_HOUR = 20;
 const WORKING_DAYS_IN_MONTH = 20;
+const WORKING_HOURS_IN_MONTH = 160;
 function getDailyEmployeeHours(empCheck) {
     let dailyHours = 0;
     switch (empCheck) {
@@ -21,12 +21,13 @@ function getDailyEmployeeHours(empCheck) {
     return dailyHours;
 }
 let totalHoursInMonth = 0;
-for (i = 0; i < WORKING_DAYS_IN_MONTH; i++) {
+let totalWorkingDays = 0;
+while (totalWorkingDays < WORKING_DAYS_IN_MONTH && totalHoursInMonth < WORKING_HOURS_IN_MONTH) {
+    totalWorkingDays++;
     empCheck = Math.floor(Math.random() * 10) % 3;
     let dailyHours = getDailyEmployeeHours(empCheck);
     let dailyWage = dailyHours * EMP_WAGE_PER_HOUR;
     console.log('Employee Daily Wage is : ' + dailyWage);
     totalHoursInMonth += dailyHours;
 }
-console.log('Employee Total Wage In A Month : ' + totalHoursInMonth * EMP_WAGE_PER_HOUR);
-
+console.log('Employee Total Wage In A Month : ' + totalHoursInMonth * EMP_WAGE_PER_HOUR + ' Working Days : ' + totalWorkingDays);
