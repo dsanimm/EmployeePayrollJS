2bf26fa9f0965bc9ebc25c675fc6e87a28b9b2ff
dsanimm <dsanimm@gmail.com>
2020-11-10 21:43:01 +0530

    added calculate wage method

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -1,7 +1,21 @@
-const IS_ABSENT=0;
-let empCheck=Math.floor(Math.random()*10)%2;
-if(empCheck==IS_ABSENT){
-    console.log('Employee is Absent !');
-}else if (empCheck==1){
-    console.log('Employee is Present !');
+//UC2 calculates the daily wage
+const IS_ABSENT = 0;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_DURATION = 4;
+const FULL_TIME_DURATION = 8;
+const EMP_WAGE_PER_HOUR = 20;
+empCheck = Math.floor(Math.random() * 10) % 3;
+let dailyHours = 0;
+let dailyWage = 0;
+switch (empCheck) {
+    case IS_ABSENT: dailyHours = 0;
+        break;
+    case IS_PART_TIME: dailyHours = PART_TIME_DURATION;
+        break;
+    case IS_FULL_TIME: dailyHours = FULL_TIME_DURATION;
+        break;
+    default: dailyHours = 0;
 }
+dailyWage = dailyHours * EMP_WAGE_PER_HOUR;
+console.log('Employee Daily Wage is : ' + dailyWage);
